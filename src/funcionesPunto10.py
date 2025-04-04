68ee15c0707ad69a727028a97578ea89a6164f31
Jonas Hariyo <jonashariyo70@gmail.com>
2025-04-04 00:15:12 -0300

    Funcionalidad de recorrer la lista por ronda y sacar los puntajes

--- src/funcionesPunto10.py
+++ src/funcionesPunto10.py
@@ -5,3 +5,25 @@ def imprimirRonda (lista,i):
         for event,value in stats.items():
             print(f" {event} : {value} ", end= " ")
         print (" ")
+
+
+def recorrerLista (lista,puntajes_totales):
+    for i in range (5):
+        puntajes_ronda = {}
+        print (" ")
+        print ("RONDA NÚMERO: ", i + 1)
+        print (" ")
+        imprimirRonda(lista,i)
+        print (" ")
+        for jugador,stats in lista[i].items():
+            puntaje = stats['kills'] * 3 + stats['assists'] * 1 - (1 if stats['deaths'] else 0)
+
+            puntajes_ronda[jugador] = puntaje
+
+            if jugador in puntajes_totales:
+                puntajes_totales[jugador]['kills'] += stats['kills']
+                puntajes_totales[jugador]['assists'] += stats['assists']
+                puntajes_totales[jugador]['deaths'] += 1 if stats['deaths'] else 0
+                puntajes_totales[jugador]['puntos'] += puntaje
+            else:
+                puntajes_totales[jugador] = {'kills': stats['kills'], 'assists': stats['assists'], 'deaths': 1 if stats['deaths'] else 0,'puntos': puntaje}
